c264376e17d4923b934d4f2576eae880229e30af
RobinMDunn <robin.dunn1012@gmail.com>
2022-03-16 22:49:53 -0400

    Partial oracle axis-aligned test

---
+++ R/partial_oracle_axis.R
@@ -0,0 +1,104 @@
+#' Title
+#'
+#' @param data
+#' @param B
+#' @param alpha
+#'
+#' @return
+#' @export
+partial_oracle_axis <- function(data, B, alpha) {
+
+  # Extract number of observations and dimension
+  n_obs <- nrow(data)
+  d <- ncol(data)
+
+  # Matrix of subsampled test statistics
+  ts_mat <- matrix(NA, nrow = B, ncol = d)
+
+  # Consider each dimension separately
+  for(d_val in 1:d) {
+
+    # Repeatedly subsample to get subsampled test statistic on each dimension
+    for(b in 1:B) {
+
+      # Split Y into Y_0 and Y_1
+      Y_0_indices <- sample(1:n_obs, size = n_obs/2)
+
+      Y_1_indices <- setdiff(1:n_obs, Y_0_indices)
+
+      Y_0 <- matrix(data[Y_0_indices, ], ncol = d)
+
+      Y_1 <- matrix(data[Y_1_indices, ], ncol = d)
+
+      # Remove previously fitted mclust_dens_D1 object
+      if(exists("mclust_dens_D1")) { rm(mclust_dens_D1) }
+
+      # Get two-component Gaussian mixture estimate for each dimension on D_1.
+      # Try unequal variance model. If an error, use equal variance model.
+      # (Any density is valid.)
+      # modelNames:
+      #   - V: unequal variance
+      #   - E: equal variance
+
+      mclust_dens_D1 <- try(mclust::densityMclust(data = Y_1[, d_val], G = 2,
+                                                  modelNames = "V",
+                                                  warn = FALSE, verbose = FALSE,
+                                                  plot = FALSE))
+      if(is(mclust_dens_D1, "try-error") | is.null(mclust_dens_D1)) {
+        mclust_dens_D1 <- try(mclust::densityMclust(data = Y_1[, d_val], G = 2,
+                                                    modelNames = "E",
+                                                    warn = FALSE,
+                                                    verbose = FALSE,
+                                                    plot = FALSE))
+      }
+
+      # If mclust_dens_D1 is not an Mclust object, fit a single Normal density.
+      # (This is very rare in simulations.)
+      if(!("Mclust" %in% class(mclust_dens_D1))) {
+        print("Fitting single Normal density")
+        mclust_dens_D1 <- try(mclust::densityMclust(data = Y_1, G = 1,
+                                                    modelNames = "V",
+                                                    warn = FALSE,
+                                                    verbose = FALSE,
+                                                    plot = FALSE))
+      }
+
+      # Evaluate Gaussian mixture on D_0
+      eval_gauss_mix_D0 <- mclust::predict.densityMclust(object = mclust_dens_D1,
+                                                         newdata = Y_0[, d_val],
+                                                         what = "dens")
+
+
+      # Stop if max evaluated density is over 100 (probably convergence issue)
+      # (Never occurs in simulations.)
+      stopifnot(max(eval_gauss_mix_D0) < 100)
+
+      # Get log-concave MLE on D_0
+      log_concave_D0 <- logcondens::logConDens(x = Y_0[, d_val])
+
+      # Evaluate log-concave MLE on D_0
+      eval_log_concave_D0 <- logcondens::evaluateLogConDens(
+        xs = Y_0[, d_val], res = log_concave_D0, which = 2)[, 3]
+
+      # Store dimension d, subsample B test stat
+      ts_mat[b, d_val] <-
+        exp(sum(log(eval_gauss_mix_D0)) - sum(log(eval_log_concave_D0)))
+
+      # Break if you would reject based on current info
+      if(sum(apply(ts_mat, MARGIN = 2, FUN = function(x) sum(x, na.rm = TRUE)) >=
+             B * d /alpha) >= 1 & b < B) {
+        ts_mat[(b+1):B, d_val] <- 0
+        break
+      }
+
+    }
+
+  }
+
+  # Get average subsampled test stat for each dimension
+  test_stats <- apply(ts_mat, MARGIN = 2, FUN = mean)
+
+  # Reject H_0 if some dimension's test_stat >= d/alpha
+  return(list(reject_null = as.numeric(sum(test_stats >= d/alpha) >= 1)))
+
+}
--- sim_code/tab01_partial_oracle_axis.R
+++ sim_code/tab01_partial_oracle_axis.R
@@ -1,19 +1,15 @@
 # Benchmark partial oracle, axis-aligned projections
 # Test H_0: log-concave versus H_1: not log-concave using universal LRT.
-# Split data into D_0 and D_1. 
+# Split data into D_0 and D_1.
 # Consider each component separately (d >= 2).
 # Get Gaussian mixture EM estimate on D_1. Get log-concave MLE on D_0.
 # Evaluate at all values in D_0.
 # Reject if T_{n,k} >= d/alpha for at least one k \in {1, 2, ..., d}.
 
-suppressMessages(library(logcondens))
-suppressMessages(library(MASS))
-suppressMessages(library(data.table))
-suppressMessages(library(progress))
-suppressMessages(library(tidyverse))
-suppressMessages(library(mclust))
+# Read in library
+library(LogConcaveUniv)
 
-# Read in arguments for file with all parameters and 
+# Read in arguments for file with all parameters and
 # line number for parameters for current simulation.
 
 parameter_file <- "sim_params/tab01_partial_oracle_axis_params.csv"
@@ -25,16 +21,16 @@ if (length(args) > 0) {
   line_number <- as.numeric(args[2])
 }
 
-parameter_df <- fread(parameter_file)
+parameter_df <- data.table::fread(parameter_file)
 
 # Assign arguments based on input.
 # Arguments are d (dimension), mu_norm, n_obs (number of obs),
-# start_sim (index of starting sim), n_sim (number of sims), 
+# start_sim (index of starting sim), n_sim (number of sims),
 # B (number of subsamples),
-# equal_space_mu (indicator for mu structure. 
+# equal_space_mu (indicator for mu structure.
 #                 If 1, each component is ||mu||*d^(-1/2).
 #                 If 0, mu = (||mu||, 0, ... 0).
-parameters <- parameter_df %>% slice(line_number)
+parameters <- parameter_df %>% dplyr::slice(line_number)
 d <- parameters$d
 mu_norm <- parameters$mu_norm
 n_obs <- parameters$n_obs
@@ -58,113 +54,44 @@ p <- 0.5
 alpha <- 0.1
 
 # Create data frame to store results
-results <- data.table(Method = "Partial oracle, axis-aligned projections",
-                      n_obs = n_obs, d = d, mu_norm = mu_norm,
-                      equal_space_mu = equal_space_mu, B = B,
-                      sim = start_sim:(start_sim + n_sim - 1), alpha = alpha,
-                      p_0 = p, time_sec = NA_real_)
+results <- data.table::data.table(
+  Method = "Partial oracle, axis-aligned projections",
+  n_obs = n_obs, d = d, mu_norm = mu_norm,
+  equal_space_mu = equal_space_mu, B = B,
+  sim = start_sim:(start_sim + n_sim - 1), alpha = alpha,
+  p_0 = p, time_sec = NA_real_)
 
 # Run simulations to check whether to reject H_0
 for(row in 1:nrow(results)) {
-  
+
   # Generate sample from two-component normal location model
   true_sample <- matrix(NA, nrow = n_obs, ncol = d)
-  
+
   for(i in 1:n_obs) {
     mixture_comp <- rbinom(n = 1, size = 1, prob = p)
     if(mixture_comp == 0) {
       true_sample[i, ] <- rnorm(n = d, mean = 0, sd = 1)
     } else if(mixture_comp == 1) {
-      true_sample[i, ] <- mvrnorm(n = 1, mu = 0 - mu, Sigma = diag(d))
+      true_sample[i, ] <- MASS::mvrnorm(n = 1, mu = 0 - mu, Sigma = diag(d))
     }
   }
-  
+
   # Get start time for simulation
   start_time <- proc.time()[3]
-  
-  # Matrix of subsampled test statistics
-  ts_mat <- matrix(NA, nrow = B, ncol = d)
-  
-  # Consider each dimension separately
-  for(d_val in 1:d) {
-    
-    # Repeatedly subsample to get subsampled test statistic on each dimension
-    for(b in 1:B) {
-      
-      # Split Y into Y_0 and Y_1
-      Y_0_indices <- sample(1:n_obs, size = n_obs/2)
-      
-      Y_1_indices <- setdiff(1:n_obs, Y_0_indices)
-      
-      Y_0 <- matrix(true_sample[Y_0_indices, ], ncol = d)
-      
-      Y_1 <- matrix(true_sample[Y_1_indices, ], ncol = d)
-      
-      # Remove previously fitted mclust_dens_D1 object
-      if(exists("mclust_dens_D1")) { rm(mclust_dens_D1) }
-      
-      # Get two-component Gaussian mixture estimate for each dimension on D_1.
-      # modelNames:
-      #   - V: unequal variance
-      #   - E: equal variance
-      
-      mclust_dens_D1 <- try(densityMclust(data = Y_1[, d_val], G = 2, 
-                                          modelNames = "V",
-                                          warn = FALSE, verbose = FALSE))
-      if(is(mclust_dens_D1, "try-error") | is.null(mclust_dens_D1)) {
-        mclust_dens_D1 <- try(densityMclust(data = Y_1[, d_val], G = 2, 
-                                            modelNames = "E",
-                                            warn = FALSE, verbose = FALSE))
-      }
-      
-      # If mclust_dens_D1 is not an Mclust object, fit a single Normal density.
-      # (This is very rare in simulations.)
-      if(!("Mclust" %in% class(mclust_dens_D1))) {
-        print("Fitting single Normal density")
-        mclust_dens_D1 <- try(densityMclust(data = Y_1, G = 1, modelNames = "V",
-                                            warn = FALSE, verbose = FALSE,
-                                            plot = FALSE))
-      }
-      
-      # Evaluate Gaussian mixture on D_0
-      eval_gauss_mix_D0 <- predict.densityMclust(object = mclust_dens_D1, 
-                                                 newdata = Y_0[, d_val], 
-                                                 what = "dens")
-      
-      # Stop if max evaluated density is over 100 (probably convergence issue)
-      # (Never occurs in simulations.)
-      stopifnot(max(eval_gauss_mix_D0) < 100)
-      
-      # Get log-concave MLE on D_0
-      log_concave_D0 <- logConDens(x = Y_0[, d_val])
-      
-      # Evaluate log-concave MLE on D_0
-      eval_log_concave_D0 <- evaluateLogConDens(
-        xs = Y_0[, d_val], res = log_concave_D0, which = 2)[, 3]
-      
-      # Store dimension d, subsample B test stat
-      ts_mat[b, d_val] <- 
-        exp(sum(log(eval_gauss_mix_D0)) - sum(log(eval_log_concave_D0)))
-      
-      # Break if you would reject based on current info
-      if(sum(apply(ts_mat, MARGIN = 2, FUN = function(x) sum(x, na.rm = T)) >= 
-             B * d /alpha) >= 1 & b < B) {
-        ts_mat[(b+1):B, d_val] <- 0
-        break
-      }
-      
-    }
-    
-  }
-  
+
+  # Run partial oracle axis-aligned projection test to determine
+  # whether to reject H_0
+  test_out <- LogConcaveUniv::partial_oracle_axis(data = true_sample, B = B,
+                                                  alpha = alpha)
+
   # Get end time for simulation
   end_time <- proc.time()[3]
-  
+
   # Store run time for simulation
   results[row, time_sec := end_time - start_time]
-  
+
 }
 
 # Save simulation results
-fwrite(results, file = paste0("sim_data/tab01_partial_oracle_axis_",
-                              line_number, ".csv"))
+data.table::fwrite(results, file = paste0("sim_data/tab01_partial_oracle_axis_",
+                                          line_number, ".csv"))
