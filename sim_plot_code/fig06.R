16c8af616f51775e1611a4abd0ea9bc4879f3c50
RobinMDunn <robin.dunn1012@gmail.com>
2022-03-22 22:04:17 -0400

    Update libraries for figure 6

--- sim_plot_code/fig06.R
+++ sim_plot_code/fig06.R
@@ -1,24 +1,26 @@
 # Create Figure 6 in appendix.
 # Plot log-concave MLE densities for normal mixtures at n = 50 and d = 1.
 
-# Read in library
-library(LogConcaveUniv)
+# Read in libraries
+suppressMessages(library(data.table))
+suppressMessages(library(tidyverse))
+suppressMessages(library(tidyr))
 
 # Create theme
-paper_theme <- ggplot2::theme_bw() +
-  ggplot2::theme(plot.title    = ggplot2::element_text(hjust = 0.5, size = 16),
-                 plot.subtitle = ggplot2::element_text(hjust = 0.5, size = 14),
-                 legend.title  = ggplot2::element_text(size = 14),
-                 axis.title    = ggplot2::element_text(size = 14),
-                 legend.text   = ggplot2::element_text(size = 12),
-                 axis.text     = ggplot2::element_text(size = 12),
-                 strip.text    = ggplot2::element_text(size = 12),
-                 panel.spacing = ggplot2::unit(1.2, "lines"))
+paper_theme <- theme_bw() +
+  theme(plot.title    = element_text(hjust = 0.5, size = 16),
+        plot.subtitle = element_text(hjust = 0.5, size = 14),
+        legend.title  = element_text(size = 14),
+        axis.title    = element_text(size = 14),
+        legend.text   = element_text(size = 12),
+        axis.text     = element_text(size = 12),
+        strip.text    = element_text(size = 12),
+        panel.spacing = unit(1.2, "lines"))
 
 # Read in data
-density_points <- data.table::fread("sim_data/fig06_points.csv")
+density_points <- fread("sim_data/fig06_points.csv")
 
-density_values <- data.table::fread("sim_data/fig06_densities.csv")
+density_values <- fread("sim_data/fig06_densities.csv")
 
 # Check parameters
 stopifnot(unique(density_values$d) == 1,
@@ -26,50 +28,49 @@ stopifnot(unique(density_values$d) == 1,
 
 # Extract (1/n)*loglik values
 loglik_df <- density_values %>%
-  dplyr::group_by(mu_norm) %>%
-  dplyr::slice(1) %>%
-  tidyr::pivot_longer(cols = c("mean_loglik_true_dens",
-                               "mean_loglik_LogConcDEAD",
-                               "mean_loglik_logcondens")) %>%
-  dplyr::mutate(name = factor(name,
-                              levels = c("mean_loglik_true_dens",
-                                         "mean_loglik_LogConcDEAD",
-                                         "mean_loglik_logcondens"),
-                              labels = c("True density",
-                                         "Log-concave MLE (LogConcDEAD)",
-                                         "Log-concave MLE (logcondens)")),
-                mu_norm = factor(mu_norm, levels = c(0, 2, 4),
-                                 labels = c("||u|| = 0", "||u|| = 2", "||u|| = 4")))
+  group_by(mu_norm) %>%
+  slice(1) %>%
+  pivot_longer(cols = c("mean_loglik_true_dens",
+                        "mean_loglik_LogConcDEAD",
+                        "mean_loglik_logcondens")) %>%
+  mutate(name = factor(name,
+                       levels = c("mean_loglik_true_dens",
+                                  "mean_loglik_LogConcDEAD",
+                                  "mean_loglik_logcondens"),
+                       labels = c("True density",
+                                  "Log-concave MLE (LogConcDEAD)",
+                                  "Log-concave MLE (logcondens)")),
+         mu_norm = factor(mu_norm, levels = c(0, 2, 4),
+                          labels = c("||u|| = 0", "||u|| = 2", "||u|| = 4")))
 
 # Plot densities
 logconc_densities_n50_d1 <- density_values %>%
-  tidyr::pivot_longer(cols = c("true_density", "LogConcDEAD_density",
-                               "logcondens_density")) %>%
-  dplyr::mutate(name = factor(name, levels = c("true_density",
-                                               "LogConcDEAD_density",
-                                               "logcondens_density"),
-                              labels = c("True density",
-                                         "Log-concave MLE (LogConcDEAD)",
-                                         "Log-concave MLE (logcondens)")),
-                mu_norm = factor(mu_norm, levels = c(0, 2, 4),
-                                 labels = c("||u|| = 0", "||u|| = 2", "||u|| = 4"))) %>%
-  ggplot2::ggplot(ggplot2::aes(x = x, y = value)) +
-  ggplot2::geom_line() +
-  ggplot2::facet_grid(mu_norm ~ name, scales = "free") +
-  ggplot2::geom_rug(ggplot2::aes(x = x), sides = "b", inherit.aes = F,
-                    data = density_points) +
-  ggplot2::geom_text(ggplot2::aes(x = -4.5, y = 0.35,
-                                  label = paste("\n", "(1/n)*loglik =",
-                                                round(value, 2))),
-                     data = loglik_df %>% dplyr::filter(d == 1, n_obs == 50)) +
+  pivot_longer(cols = c("true_density", "LogConcDEAD_density",
+                        "logcondens_density")) %>%
+  mutate(name = factor(name, levels = c("true_density",
+                                        "LogConcDEAD_density",
+                                        "logcondens_density"),
+                       labels = c("True density",
+                                  "Log-concave MLE (LogConcDEAD)",
+                                  "Log-concave MLE (logcondens)")),
+         mu_norm = factor(mu_norm, levels = c(0, 2, 4),
+                          labels = c("||u|| = 0", "||u|| = 2", "||u|| = 4"))) %>%
+  ggplot(aes(x = x, y = value)) +
+  geom_line() +
+  facet_grid(mu_norm ~ name, scales = "free") +
+  geom_rug(aes(x = x), sides = "b", inherit.aes = F,
+           data = density_points) +
+  geom_text(aes(x = -4.5, y = 0.35,
+                label = paste("\n", "(1/n)*loglik =", round(value, 2))),
+            data = loglik_df %>% filter(d == 1, n_obs == 50)) +
   paper_theme +
-  ggplot2::labs(y = "Density",
-                title = "True density and log-concave MLE estimates, n = 50, d = 1")
+  labs(y = "Density",
+       title = "True density and log-concave MLE estimates, n = 50, d = 1")
 
 #####################
 ##### Save plot #####
 #####################
 
-ggplot2::ggsave(plot = logconc_densities_n50_d1,
-                filename = "sim_plots/figure_06.pdf",
-                width = 9.7, height = 5)
+ggsave(plot = logconc_densities_n50_d1,
+       filename = "sim_plots/figure_06.pdf",
+       width = 9.7, height = 5)
