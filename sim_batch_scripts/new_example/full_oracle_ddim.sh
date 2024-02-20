2ba124b50565458d0ce8d2922d0e36210bdc876d
RobinMDunn <robin.dunn1012@gmail.com>
2024-02-19 20:39:25 -0500

    Params and sh script for full oracle d-dim sims

---
+++ sim_batch_scripts/new_example/full_oracle_ddim.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+#SBATCH -t 100:00:00
+#SBATCH -p RM-shared
+#SBATCH -N 1
+#SBATCH --ntasks-per-node 1
+#SBATCH --array=1-10:1
+
+Rscript sim_code/new_example/sim_full_oracle_ddim.R sim_params/new_example/full_oracle_ddim_params.csv $SLURM_ARRAY_TASK_ID
+
