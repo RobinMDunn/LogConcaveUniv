b98f0ca4105d80e84423bc81216c103ae8ab92e7
RobinMDunn <robin.dunn1012@gmail.com>
2024-02-19 20:35:48 -0500

    Code for full oracle d-dim sims on non-LC normal mixture example, restricted and unrestricted domains

---
+++ sim_code/new_example/sim_full_oracle_ddim.R
@@ -0,0 +1,96 @@
+# Test H_0: log-concave versus H_1: not log-concave using universal LRT.
+# Split data into D_0 and D_1.
+# Use true density as numerator. Get log-concave MLE on D_0.
+# Evaluate at all values in D_0.
+# Density is 0.5*N(0, I_2) + 0.5*N(0, sigma^2 I_2), where sigma = sqrt(3).
+
+# Read in library
+# library(LogConcaveUniv)
+devtools::load_all()
+
+# Read in arguments for file with all parameters and
+# line number for parameters for current simulation.
+
+parameter_file <- "sim_params/new_example/full_oracle_ddim_params.csv"
+line_number <- 1
+
+args <- commandArgs(trailingOnly = TRUE)
+if (length(args) > 0) {
+  parameter_file <- args[1]
+  line_number <- as.numeric(args[2])
+}
+
+parameter_df <- data.table::fread(parameter_file)
+
+# Assign arguments based on input.
+# Arguments are d (dimension),
+# sigma (generating from (1-p) N(0, I_d) + p N(0, sigma^2 I_d)),
+# n_obs (number of obs),
+# start_sim (index of starting sim), n_sim (number of sims),
+# B (number of subsamples),
+# compute_ts (indicator for whether to compute test stat.
+#             If 1, computes test stat.
+#             If 0, does not compute test stat, allowing for early rejection.)
+
+parameters <- parameter_df %>% dplyr::slice(line_number)
+d <- parameters$d
+sigma <- parameters$sigma
+n_obs <- parameters$n_obs
+start_sim <- parameters$start_sim
+n_sim <- parameters$n_sim
+B <- parameters$B
+compute_ts <- parameters$compute_ts
+
+# Create mu vector. (mu is the 0 vector for both components.)
+mu <- rep(0, d)
+
+# Proportions for the two Gaussian components
+p <- 0.5
+
+# Alpha level
+alpha <- 0.1
+
+# Create data frame to store results
+results <- data.table::data.table(n_obs = n_obs, d = d,
+                                  sigma = sigma, B = B,
+                                  sim = start_sim:(start_sim + n_sim - 1),
+                                  alpha = alpha, p_0 = p, test_stat = NA_real_,
+                                  reject = NA_real_)
+
+# Set up progress bar
+pb <- progress::progress_bar$new(format = "sim :current / :total [:bar] :eta",
+                                 total = nrow(results),
+                                 clear = T, show_after = 0)
+
+# Run simulations to check whether to reject H_0
+for(row in 1:nrow(results)) {
+
+  # Increment progress bar
+  pb$tick()
+
+  # Generate sample from two-component normal location model
+  true_sample <- matrix(NA, nrow = n_obs, ncol = d)
+
+  for(i in 1:n_obs) {
+    mixture_comp <- rbinom(n = 1, size = 1, prob = p)
+    if(mixture_comp == 0) {
+      true_sample[i, ] <- rnorm(n = d, mean = 0, sd = 1)
+    } else if(mixture_comp == 1) {
+      true_sample[i, ] <- rnorm(n = d, mean = 0, sd = sigma)
+    }
+  }
+
+  # Run full oracle d-dimensional test to determine whether to reject H_0
+  test_out <- LogConcaveUniv::full_oracle_ddim(data = true_sample, B = B,
+                                               alpha = alpha, mu = mu,
+                                               sigma = sigma, p = p,
+                                               compute_ts = compute_ts)
+
+  results[row, test_stat := test_out$test_stat]
+  results[row, reject := test_out$reject_null]
+
+}
+
+# Save simulation results
+data.table::fwrite(results, file = paste0("sim_data/new_example/full_oracle_ddim_",
+                                          line_number, ".csv"))
