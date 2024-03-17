f75b7a1508f8e6b688433391b8d6cce8ae2db089
RobinMDunn <robin.dunn1012@gmail.com>
2024-03-16 22:05:47 -0400

    Code and params for new partial oracle ddim sims

--- sim_code/new_example/sim_partial_oracle_ddim.R
+++ sim_code/new_example/sim_partial_oracle_ddim.R
@@ -7,16 +7,19 @@
 # Read in library
 library(LogConcaveUniv)
 
-# Read in arguments for file with all parameters and
-# line number for parameters for current simulation.
+# Read in arguments for file with all parameters,
+# line number for parameters for current simulation, and
+# number of parallel cores to use in simulation.
 
 parameter_file <- "sim_params/new_example/partial_oracle_ddim_params.csv"
 line_number <- 1
+n_cores <- 1
 
 args <- commandArgs(trailingOnly = TRUE)
 if (length(args) > 0) {
   parameter_file <- args[1]
   line_number <- as.numeric(args[2])
+  n_cores <- as.numeric(args[3])
 }
 
 parameter_df <- data.table::fread(parameter_file)
@@ -53,25 +56,18 @@ alpha <- 0.1
 results <- data.table::data.table(n_obs = n_obs, d = d,
                                   sigma = sigma, B = B,
                                   sim = start_sim:(start_sim + n_sim - 1),
-                                  alpha = alpha, p_0 = p, test_stat = NA_real_,
-                                  reject = NA_real_)
+                                  alpha = alpha, p_0 = p,
+                                  compute_ts = compute_ts)
 
-# Set up progress bar
-pb <- progress::progress_bar$new(format = "sim :current / :total [:bar] :eta",
-                                 total = nrow(results),
-                                 clear = T, show_after = 0)
-
-# Run simulations to check whether to reject H_0
-for(row in 1:nrow(results)) {
-
-  #Increment progression bar
-  pb$tick()
+# Code to run one simulation to check whether to reject H_0
+one_sim_partial_oracle_ddim <- function(n_obs, d, sigma, B, sim, 
+                                        alpha, p_0, compute_ts) {
 
   # Generate sample from two-component normal location model
   true_sample <- matrix(NA, nrow = n_obs, ncol = d)
 
   for(i in 1:n_obs) {
-    mixture_comp <- rbinom(n = 1, size = 1, prob = p)
+    mixture_comp <- rbinom(n = 1, size = 1, prob = p_0)
     if(mixture_comp == 0) {
       true_sample[i, ] <- rnorm(n = d, mean = 0, sd = 1)
     } else if(mixture_comp == 1) {
@@ -84,11 +80,25 @@ for(row in 1:nrow(results)) {
                                                   alpha = alpha,
                                                   compute_ts = compute_ts)
 
-  results[row, test_stat := test_out$test_stat]
-  results[row, reject := test_out$reject_null]
+  return(test_out)
 
 }
 
+# Run simulations to check whether to reject H_0, iterating over rows of results
+test_out <- clustermq::Q_rows(df = results, fun = one_sim_partial_oracle_ddim, 
+                              n_jobs = n_cores)
+
+# Append outputs to results df
+results$avg_ts <- sapply(test_out, FUN = function(x) x$test_stat)
+results$reject <- sapply(test_out, FUN = function(x) x$reject_null)
+
+# Create aggregated results (number of rejections at given parameters)
+results_agg <- results %>% 
+  dplyr::group_by(n_obs, d, sigma, B, alpha, p_0) %>% 
+  dplyr::summarize(n_reject = sum(reject), 
+                   n_sim = dplyr::n())
+
 # Save simulation results
-data.table::fwrite(results, file = paste0("sim_data/new_example/partial_oracle_ddim_",
-                                          line_number, ".csv"))
+data.table::fwrite(results_agg, 
+                   file = paste0("sim_data/new_example/partial_oracle_ddim_",
+                                 line_number, ".csv"))
